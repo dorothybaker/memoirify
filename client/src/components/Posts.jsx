fdff894813112e60ad75e74c153b1c1772f6f317
Dorothy Baker <tbakerdorothy@gmail.com>
2024-04-19 15:02:49 +0300

    data

--- client/src/components/Posts.jsx
+++ client/src/components/Posts.jsx
@@ -5,7 +5,7 @@ function Posts({ posts }) {
     <div className="flex-4 flex flex-col gap-3">
       <h2 className="font-medium uppercase">Recent Posts</h2>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 lg:gap-3 md:gap-3 gap-4">
-        {posts?.slice(0, 8)?.map((post) => (
+        {posts?.map((post) => (
           <Post key={post._id} post={post} />
         ))}
       </div>
